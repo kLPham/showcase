023cfe1952fdf587c10acd966abe545721008e2c
Kelly Pham <Kellylpham@gmail.com>
2017-11-05 12:59:26 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+
+
+
 
 class App extends Component {
-  render() {
-    
+  render(){
+    return(
+      <TopicBrowser />
+      
+    )
   }
+    
 }
+   
+
 
 export default App;
